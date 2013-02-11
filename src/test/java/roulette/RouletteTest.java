7ab0281c5b5a1cb69eb587d2aff002e49978edce
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-11 21:56:41 +0100

    behaviourtested random

--- src/test/java/roulette/RouletteTest.java
+++ src/test/java/roulette/RouletteTest.java
@@ -1,15 +1,31 @@
 package roulette;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.junit.Test;
 
 public class RouletteTest {
 
+    @Test public void 
+    test_behaviour_of_random() throws Exception {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10000; i++) {
+            int nextInt = new Random().nextInt(37);
+            list.add(nextInt);
+            assertThat(nextInt).isGreaterThanOrEqualTo(0);
+            assertThat(nextInt).isLessThanOrEqualTo(36);
+        }
+        assertThat(list).contains(36);
+        assertThat(list).contains(0);
+    }
+    
     @Test
     public void it_returns_a_random_number_that_can_be_0() {
         Random mock = mock(Random.class);
